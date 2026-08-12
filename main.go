2655b0e4d7e2b01b5543f00dcdec5672a7b38590
Ethan <p.ethanpod@gmail.com>
2026-08-12 09:20:22 -0400

    I am learning something new: how to connect a database to a program. They never taught me how to do this in college... I am confused but I will figure it out

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	//_ "github.com/lib/pq"
+)
+
+func main() {
+
+	connString := "host=localhost port=5000 user=postgres password=dissys dbname=hashdb sslmode = disable"
+
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("Connection failed: ", err)
+	}
+
+	fmt.Println("Connected to Database!")
+}
