82f2c54c9238ff90c996cfdc248b332c84abfa2c
Ethan <p.ethanpod@gmail.com>
2026-08-18 12:09:06 -0400

    still playing around with databases. Trying to get back in the swing of things with working on projects.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module consistent-hashing-proxy
+
+go 1.26.4
+
+require github.com/mattn/go-sqlite3 v1.14.50 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
-	//_ "github.com/lib/pq"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 
-	connString := "host=localhost port=5000 user=postgres password=dissys dbname=hashdb sslmode = disable"
+	fmt.Println("Available drivers:", sql.Drivers())
 
-	db, err := sql.Open("postgres", connString)
+	db, err := sql.Open("sqlite3", "test.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	if err := db.Ping(); err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
 		log.Fatal("Connection failed: ", err)
 	}
 
